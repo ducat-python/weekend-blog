44bb0fb789fc0a596af55b059c741e1761a028c6
Piyush <piyush.maurya@outlook.in>
2017-07-08 11:59:01 +0530

    Blog Create and form added

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import blog_list
+from .views import blog_list, blog_create
 
 urlpatterns = [
 
     url(r'^list/', blog_list, name='list'),
+    url(r'^create/', blog_create, name='create'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
 
 from .models import Blog
+from .forms import BlogForm
 
-
+#CRUD
 # Create your views here.
 
 
@@ -13,3 +15,20 @@ def blog_list(request):
     }
 
     return render(request, 'blogs/list.html', context)
+
+
+
+def blog_create(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            return HttpResponseRedirect(reverse('blogs:list'))
+    else:
+        form = BlogForm()
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'blogs/create.html', context)
